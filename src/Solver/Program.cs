c987c87934613ea9905b967085eed4cb21c74172
cashto <cashto@gmail.com>
2019-06-20 22:11:49 -0700

    Initial commit

---
+++ src/Solver/Program.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+    }
+
+    public static int Add(int x, int y)
+    {
+        return x + y;
+    }
+}
---
+++ src/UnitTests/Tests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class Tests
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        Assert.AreEqual(4, Program.Add(2, 2));
+    }
+}
